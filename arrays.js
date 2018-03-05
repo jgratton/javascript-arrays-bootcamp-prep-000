beb1fee457ac562a48cb2bcb000189a34afc6126
John Gratton <jgratton@gmail.com>
2018-03-05 16:43:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
+
+function destructivelyaddElementToBeginningOfArray(array, element) {
+  array = [element, ...array];
+  return array;
+}
