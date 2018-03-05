a3d5406538bb446b5d90caacab5012e2e9b4ad94
John Gratton <jgratton@gmail.com>
2018-03-05 16:46:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array = [element, ...array];
   return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
